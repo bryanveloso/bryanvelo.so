916dabd2a51915ee8c03bb2352d668e4c33ca307
Bryan Veloso <bryan@velo.so>
2021-10-14 08:57:22 -0700

    Let's just redirect to our YouTube channel.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const YOUTUBE_URL = 'https://www.youtube.com/channel/UCcpyzHIletIDFfin6ZIv10A'
+
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: YOUTUBE_URL,
+        permanent: false,
+        basePath: false
+      },
+    ]
+  }
 }
