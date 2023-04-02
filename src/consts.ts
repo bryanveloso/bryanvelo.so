53b2c887f0f0a28469a1c7d1b6e1f2ecf4c3ba70
Bryan Veloso <bryan@velo.so>
2023-04-02 09:49:42 -0700

    New constants.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Navigating the Stars of Avalon'
+export const SITE_DESCRIPTION =
+  'My name is Bryan Veloso, and these are my memoirs.'
